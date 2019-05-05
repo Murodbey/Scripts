b41ff887fda4b87d16d4a1a85ec41c42ab0e388d
Murodbey <abdullayevmurod@gmail.com>
2019-05-05 18:09:43 -0500

    useradd

--- Useradd-script/useradd.sh
+++ Useradd-script/useradd.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-for i in $( cat userslist.txt ); do useradd $i ;
-do echo 'redhat' | passwd --stdin $i;
-do echo  "$i ALL=(ALL) NOPASSWD: ALL " >> /etc/sudoers ;
+if [ $(id -u) -eq 0 ]; then
+NEW_USERS="/userslist.txt"
+HOME_BASE="/home/"
+cat ${NEW_USERS} |
+while read USER PASSWORD GROUP
+do
+pass=$(perl -e 'print crypt($ARGV[0], "password")' $PASSWORD)
+egrep "^$USER[0]" /etc/passwd > /dev/null
+if [ $? -eq 0 ]; then
+echo "User $USER exists!"
+exit 1
+else
+useradd -g ${GROUP} -p ${pass} -m -d ${HOME_BASE}${USER} ${USER}
+[ $? -eq 0 ] && echo "User $USER has been added to the system" || echo "Failed adding user $USER!"
+fi
 done
+fi
