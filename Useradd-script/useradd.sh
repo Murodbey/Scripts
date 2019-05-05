0e4ab86e06cd08653af1e2c44467be9d16436708
Murodbey <abdullayevmurod@gmail.com>
2019-05-05 18:18:29 -0500

    updated

--- Useradd-script/useradd.sh
+++ Useradd-script/useradd.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-if [ $(id -u) -eq 0 ]; then
-NEW_USERS="userslist.txt"
-HOME_BASE="/home/"
-cat ${NEW_USERS} |
-while read USER PASSWORD GROUP
+# user account create via scripti file
+
+file=userlist.txt
+user=$(cat "file")
+
+for i in $user
 do
-pass=$(perl -e 'print crypt($ARGV[0], "password")' $PASSWORD)
-egrep "^$USER[0]" /etc/passwd > /dev/null
-if [ $? -eq 0 ]; then
-echo "User $USER exists!"
-exit 1
-# else
-# useradd -g ${GROUP} -p ${pass} -m -d ${HOME_BASE}${USER} ${USER}
-# [ $? -eq 0 ] && echo "User $USER has been added to the system" || echo "Failed adding user $USER!"
-# fi
-# done
-# fi
+useradd $i
+echo "$i ALL=(ALL) NOPASSWD: ALL " >> /etc/sudoers ;
+done
