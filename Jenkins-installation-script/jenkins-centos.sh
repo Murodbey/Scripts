f6f7d116a4e977b16dc94cff0897dfc440ad2fa9
Murodbey <abdullayevmurod@gmail.com>
2019-05-06 14:11:00 -0500

    updated

--- Jenkins-installation-script/jenkins-centos.sh
+++ Jenkins-installation-script/jenkins-centos.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+user=jenkins
+for i in $user
+sudo useradd $i
+
+read -p "Please enter a  password for $i:" password
+echo "passwd" | passwd jenkins --stdin
 sudo yum install java-1.8.0-openjdk-devel -y
 curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
@@ -6,3 +12,6 @@ sudo yum install jenkins -y
 sudo systemctl start jenkins
 systemctl status jenkins
 sudo systemctl enable jenkins
+sudo su - jenkins -s /bin/bash
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+read -p "Please enter a  password for $i:" password
