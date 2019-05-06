a1f4c26752463a12ccd7fb362d5bb42725085656
Murodbey <abdullayevmurod@gmail.com>
2019-05-06 13:54:10 -0500

    updated

--- Jenkins-installation-script/jenkins-centos.sh
+++ Jenkins-installation-script/jenkins-centos.sh
@@ -2,7 +2,7 @@
 sudo yum install java-1.8.0-openjdk-devel
 curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum install jenkins
+sudo yum install jenkins -y
 sudo systemctl start jenkins
 systemctl status jenkins
 sudo systemctl enable jenkins
