5593582dec1e4869c755c28b75f42f5e41e946d9
Murodbey <abdullayevmurod@gmail.com>
2019-05-08 16:59:47 -0500

    script updated

--- Useradd-script/useradd.sh
+++ Useradd-script/useradd.sh
@@ -6,6 +6,7 @@ export FILE=`cat userslist.txt`
 for i in $FILE
 do
 useradd $i
+mkdir /home/$i/.ssh ;
+cp -rf authorized_keys /home/$i/.ssh/ ;
 echo "$i ALL=(ALL) NOPASSWD: ALL " >> /etc/sudoers ;
-echo "redhat"  | passwd $i --stdin
 done 
