6cfaaad3c6e3ae8cabcd560e6d9095d30cc42512
Murodbey <abdullayevmurod@gmail.com>
2019-07-11 22:43:44 -0500

    docker installation script

---
+++ Docker-Docker-compose-installation-script/docker-centos.sh
@@ -0,0 +1,12 @@
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce
+sudo usermod -aG docker $(whoami)
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+sudo yum install epel-release -y
+sudo yum install -y python-pip
+sudo pip install docker-compose -y
+sudo yum upgrade python*
+docker-compose version
