e33895d20bd01ae874b8eb5204b87ba70b5fb86b
Murodbey <abdullayevmurod@gmail.com>
2019-07-11 22:47:45 -0500

    updated

--- Docker-Docker-compose-installation-script/docker-centos.sh
+++ Docker-Docker-compose-installation-script/docker-centos.sh
@@ -1,12 +1,12 @@
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2 -y
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce
+sudo yum install docker-ce -y
 sudo usermod -aG docker $(whoami)
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
 sudo yum install epel-release -y
 sudo yum install -y python-pip
-sudo pip install docker-compose -y
+sudo pip install docker-compose
 sudo yum upgrade python*
 docker-compose version
