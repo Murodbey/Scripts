9857d32616b177202447046235172843f27a50fa
Murodbey <abdullayevmurod@gmail.com>
2020-12-01 14:48:31 -0600

    jenkins script updated

--- Jenkins-installation-script/jenkins-centos.sh
+++ Jenkins-installation-script/jenkins-centos.sh
@@ -10,7 +10,7 @@ sudo yum install java-1.8.0-openjdk-devel -y
 curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 
-sudo yum install jenkins -y
+sudo yum install jenkins --nogpgcheck
 sudo systemctl start jenkins
 systemctl status jenkins
 sudo systemctl enable jenkins
